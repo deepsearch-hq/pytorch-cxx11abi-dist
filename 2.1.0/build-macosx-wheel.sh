8d9a7fd7b02b0b1a4fe14e6f6b06655c8ad37877
chris-deepsearch <131630453+chris-deepsearch@users.noreply.github.com>
2023-10-13 17:17:14 +0900

    2.1.0 wheels added

---
+++ 2.1.0/build-macosx-wheel.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -ex
+
+WORKING_DIR=./tmp
+
+CFLAGS="$CFLAGS -Wno-incompatible-pointer-types -Wno-sign-compare -Wno-nonnull -Wno-unused-command-line-argument"
+CXXFLAGS="-D_GLIBCXX_USE_CXX11_ABI=1 -Wno-unused-result -Wno-cast-function-type-strict -Wno-uninitialized -Wno-nonnull -Wno-strict-aliasing -Wno-deprecated-declarations -Wno-free-nonheap-object -Wno-reorder-init-list -Wno-unused-but-set-variable -Wno-unused-command-line-argument"
+
+mkdir -p $WORKING_DIR
+pushd $WORKING_DIR
+git clone --depth 1 -b v2.1.0 https://github.com/pytorch/pytorch.git \
+    && cd pytorch \
+    && sed -i "" '/cast-function-type/d' CMakeLists.txt \
+    && git submodule sync \
+    && git submodule update --init --recursive --jobs `nproc --all` \
+    && CFLAGS=$CFLAGS CXXFLAGS=$CXXFLAGS PYTORCH_BUILD_VERSION=2.1.0 \
+       USE_PYTORCH_QNNPACK=0 USE_NNPACK=0 USE_QNNPACK=0 USE_MPS=0 \
+       USE_XNNPACK=0 PYTORCH_BUILD_NUMBER=1 python3 setup.py bdist_wheel \
+    && cd ..
+
+mv ./pytorch/dist/torch-2.1.0-cp311-cp311-macosx_*.whl ../
+popd
+rm -rf $WORKING_DIR
